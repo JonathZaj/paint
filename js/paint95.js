175b6998e206b7c88083291217ffa1a0364f403a
Jonathan Zajfen <jonathan.zajfen@gmail.com>
2018-11-11 00:29:19 +0200

    Save and load

--- js/paint95.js
+++ js/paint95.js
@@ -4,7 +4,6 @@ paint.colors = ["red", "blue", "yellow", "green", "black"];
 
 paint.selectedColor = "black";
 paint.selectedSize = "20";
-console.log(paint.selectedColor);
 
 paint.generateColorsButton = function () {
     for (var i = 0; i < paint.colors.length; i++) {
@@ -15,14 +14,14 @@ paint.generateColorsButton = function () {
         buttonItem.id = paint.colors[i];
         buttonItem.appendChild(newButton);
         document.getElementById("colors-menu").appendChild(buttonItem);
+        buttonItem.addEventListener('click', function (e) {
+            paint.selectedColor = this.id;
+        })
     }
-    buttonItem.addEventListener('click', function (e) {
-        paint.selectedColor = this.id;
-    })
 }
 
-paint.selectRubber = function(){
-    document.getElementById("rubber").addEventListener("click", function(){
+paint.selectRubber = function () {
+    document.getElementById("rubber").addEventListener("click", function () {
         paint.selectedColor = "white";
     })
 }
@@ -56,17 +55,61 @@ document.addEventListener("click", function () {
     document.getElementById("canvas").removeEventListener("mousemove", paint.startDrawing);
 });
 
-paint.clearPainting = function(){
-    document.getElementById("clear").addEventListener("click", function(){
+paint.clearPainting = function () {
+    document.getElementById("clear").addEventListener("click", function () {
         var canvas = document.getElementById("canvas");
         var drawing = canvas.getElementsByTagName('div');
-        while(drawing.length>0){
+        while (drawing.length > 0) {
             canvas.removeChild(drawing[0]);
         }
     })
 }
 
+paint.save = function () {
+    var canvas = document.getElementById("canvas");
+    var canvasLeft = canvas.getBoundingClientRect().left;
+    var canvasTop = canvas.getBoundingClientRect().top;
+    var canvasObj = {};
+    canvasObj["dots"] = [];
+    var allDots = canvas.getElementsByTagName('div');
+    for (var i = 0; i < allDots.length; i++) {
+        var currentDot = allDots[i];
+        var dotObj = {};
+        dotObj["color"] = currentDot.style.backgroundColor;
+        dotObj["size"] = currentDot.clientHeight;
+        dotObj["top"] = currentDot.getBoundingClientRect().top - canvasTop;
+        dotObj["left"] = currentDot.getBoundingClientRect().left - canvasLeft;
+        canvasObj["dots"].push(dotObj);
+    }
+    localStorage.setItem('drawing', JSON.stringify(canvasObj));
+    alert("Drawing Saved");
+}
 
+paint.load = function () {
+    var loadedDrawing = localStorage.getItem('drawing');
+    var canvasObj = JSON.parse(loadedDrawing);
+    var canvas = document.getElementById("canvas");
+    var drawing = canvas.getElementsByTagName('div');
+    while (drawing.length > 0) {
+        canvas.removeChild(drawing[0]);
+    }
+    var allDots = canvasObj["dots"];
+    for (var i = 0; i < allDots.length; i++) {
+        var currentDot = allDots[i];
+        var newDot = document.createElement("div");
+        newDot.style.backgroundColor = currentDot["color"];
+        newDot.clientHeight = currentDot["size"];
+        newDot.offsetWidth = currentDot["size"];
+        newDot.borderRadius = "50%"
+        document.getElementById("canvas").appendChild(newDot);
+        newDot.style.top = currentDot["top"] + "px";
+        newDot.style.left = currentDot["left"] + "px";
+    }
+    alert("Drawing Loaded");
+}
+
+document.getElementById('save').addEventListener("click", paint.save);
+document.getElementById('load').addEventListener("click", paint.load);
 
 paint.generateColorsButton();
 paint.selectSize();
